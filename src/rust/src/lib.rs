def6fd8b049dc5d811800c0136030a3c00dacb47
HideBa <baba.papa1120.ba@gmail.com>
2024-11-23 21:03:08 +0100

    wip: writing test for write file

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -1,11 +1,18 @@
-mod cityjson;
+#![allow(clippy::manual_range_contains)]
+
 mod error;
+#[allow(dead_code, unused_imports, clippy::all, warnings)]
 mod feature_generated;
 mod feature_writer;
 mod file_writer;
+#[allow(dead_code, unused_imports, clippy::all, warnings)]
 mod header_generated;
 
+pub use cjseq::*;
 pub use file_writer::*;
+
+// pub use feature_generated::*;
+// pub use header_generated::*;
 
 pub const VERSION: u8 = 1;
 pub(crate) const MAGIC_BYTES: [u8; 8] = [b'F', b'C', b'B', VERSION, b'1', b'0', b'0', b'0'];
