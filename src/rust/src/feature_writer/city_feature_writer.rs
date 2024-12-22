1ae7ea85dad2df33107af7bce028fc595eceb455
HideBa <baba.papa1120.ba@gmail.com>
2024-12-22 10:52:40 +0100

    feat: encode boundaries of geometry

--- src/rust/src/feature_writer/city_feature_writer.rs
+++ src/rust/src/feature_writer/city_feature_writer.rs
@@ -1,12 +1,16 @@
+use serde_json::value::Index;
+
 use super::FeatureWriter;
 use crate::{
     feature_generated::{
-        CityFeature, CityFeatureArgs, CityObject, CityObjectArgs, CityObjectType, Geometry,
-        GeometryType, SemanticObject, SemanticSurfaceType, Vertex,
+        CityFeature, CityFeatureArgs, CityObject, CityObjectArgs, CityObjectType, Geometry, GeometryArgs, GeometryType, SemanticObject, SemanticSurfaceType, Vertex
     },
     header_generated::GeographicalExtent,
     Column,
 };
+
+use cjseq::{Boundaries}
+
 
 impl<'a> FeatureWriter<'a> {
     fn create_city_object_type(&self, co_type: &str) -> CityObjectType {
@@ -170,22 +174,22 @@ impl<'a> FeatureWriter<'a> {
         &mut self,
         geometry_type: &str,
         lod: &str,
-        boundaries: &[u32],
+        boundaries: Boundaries,
         semantics: &[u32],
         semantics_objects: &[flatbuffers::WIPOffset<SemanticObject<'a>>],
     ) -> flatbuffers::WIPOffset<Geometry<'a>> {
         let type_ = self.create_geometry_type(geometry_type);
         let lod = Some(self.fbb.create_string(lod));
 
-        let encoder_decoder = GeometryEncoderDecoder::encode(boundaries);
-        let (solids, shells, surfaces, strings) = encoder_decoder.decode();
+        let encoder_decoder = GeometryEncoderDecoder::new().encode(boundaries);
+        let [solids, shells, surfaces, strings, boundary_indices] = encoder_decoder.values();
 
-        // let solids = Some(self.fbb.create_vector(solids));
-        // let shells = Some(self.fbb.create_vector(shells));
-        // let surfaces = Some(self.fbb.create_vector(surfaces));
-        // let strings = Some(self.fbb.create_vector(strings));
+        let solids = Some(self.fbb.create_vector(solids));
+        let shells = Some(self.fbb.create_vector(shells));
+        let surfaces = Some(self.fbb.create_vector(surfaces));
+        let strings = Some(self.fbb.create_vector(strings));
+        let boundary_indices = Some(self.fbb.create_vector(boundary_indices));
 
-        let boundaries = Some(self.fbb.create_vector(boundaries));
         let semantics = Some(self.fbb.create_vector(semantics));
         let semantics_objects = Some(self.fbb.create_vector(semantics_objects));
 
@@ -198,14 +202,13 @@ impl<'a> FeatureWriter<'a> {
                 shells,
                 surfaces,
                 strings,
-                boundaries,
+                boundaries: boundary_indices,
                 semantics,
                 semantics_objects,
             },
         )
     }
 }
-
 pub struct GeometryEncoderDecoder {
     solids: Vec<u32>,
     shells: Vec<u32>,
@@ -216,22 +219,182 @@ pub struct GeometryEncoderDecoder {
 }
 
 impl GeometryEncoderDecoder {
-    pub fn encode(&mut self, boundaries: &[u32]) -> Self {
-        todo!("encode boundaries")
+    pub fn new() -> Self {
+        Self {
+            solids: vec![],
+            shells: vec![],
+            surfaces: vec![],
+            strings: vec![],
+            indices: vec![],
+            semantics: vec![],
+        }
     }
 
-    pub fn values(&self) -> [&Vec<u32>; 6] {
+    fn encode_boundaries(&mut self, boundaries: &Boundaries) -> usize {
+        match boundaries {
+            Boundaries::Indices(indices) => {
+                self.indices.extend_from_slice(&indices);
+                self.strings.push(self.indices.len() as u32);
+                1
+            }
+            Boundaries::Nested(boundaries) => {
+                let mut max_depth = 0;
+                for sub in boundaries {
+                    let d = self.encode_boundaries(sub);
+                    if d > max_depth {
+                        max_depth = d;
+                    }
+                }
+
+                let count = boundaries.len() as u32;
+                match max_depth {
+                    1 => self.surfaces.push(count),
+                    2 => self.shells.push(count),
+                    3 => self.solids.push(count),
+                    _ => unreachable!(),
+                }
+                max_depth + 1
+            }
+        }
+    }
+    pub fn encode(mut self, boundaries: Boundaries) -> Self {
+        self.encode_boundaries(&boundaries);
+        self
+    }
+
+
+    pub fn values(&self) -> [&[u32]; 5] {
         [
-            self.solids,
-            self.shells,
-            self.surfaces,
-            self.strings,
-            self.indices,
-            self.semantics,
+            &self.solids,
+            &self.shells,
+            &self.surfaces,
+            &self.strings,
+            &self.indices,
         ]
     }
 
-    pub fn decode(self) -> Vec<u32> {
-        todo!("decode boundaries")
+    pub fn decode(self) -> Boundaries {
+        let mut shell_cursor = 0;
+        let mut surface_cursor = 0;
+        let mut ring_cursor = 0;
+        let mut index_cursor = 0;
+
+        if !self.solids.is_empty() {
+            let mut solids_vec = Vec::new();
+            for &shell_count in &self.solids {
+                let mut shell_vec = Vec::new();
+                for _ in 0..shell_count {
+                    let surfaces_in_shell = self.shells[shell_cursor] as usize;
+                    shell_cursor += 1;
+
+                    let mut surface_vec = Vec::new();
+                    for _ in 0..surfaces_in_shell {
+                        let rings_in_surface = self.surfaces[surface_cursor] as usize;
+                        surface_cursor += 1;
+
+                        let mut ring_vec = Vec::new();
+                        for _ in 0..rings_in_surface {
+                            let ring_size = self.strings[ring_cursor] as usize;
+                            ring_cursor += 1;
+
+                            let ring_indices = self.indices[index_cursor..index_cursor + ring_size as usize]
+                                .iter()
+                                .map(|x| *x as usize)
+                                .collect::<Vec<_>>();
+                            index_cursor += ring_size as usize;
+
+                            let ring_indices = ring_indices.into_iter()
+                                .map(|x| x as u32)
+                                .collect::<Vec<_>>();
+                            ring_vec.push(Boundaries::Indices(ring_indices));
+                        }
+
+                        surface_vec.push(Boundaries::Nested(ring_vec));
+                    }
+
+                    shell_vec.push(Boundaries::Nested(surface_vec));
+                }
+
+                solids_vec.push(Boundaries::Nested(shell_vec));
+            }
+
+            if solids_vec.len() == 1 {
+                solids_vec.into_iter().next().unwrap()
+            } else {
+                Boundaries::Nested(solids_vec)
+            }
+        }
+        else if !self.shells.is_empty() {
+            let mut shell_vec = Vec::new();
+            for &surface_count in &self.shells {
+                let mut surface_vec = Vec::new();
+                for _ in 0..surface_count {
+                    let rings_in_surface = self.surfaces[surface_cursor] as usize;
+                    surface_cursor += 1;
+
+                    let mut ring_vec = Vec::new();
+                    for _ in 0..rings_in_surface {
+                        let ring_size = self.strings[ring_cursor] as usize;
+                        ring_cursor += 1;
+                        let ring_indices = self.indices[index_cursor..index_cursor + ring_size as usize]
+                            .iter()
+                            .map(|x| *x as usize)
+                            .collect::<Vec<_>>();
+                        index_cursor += ring_size as usize;
+
+                        ring_vec.push(Boundaries::Indices(ring_indices.into_iter().map(|x| x as u32).collect()));
+                    }
+                    surface_vec.push(Boundaries::Nested(ring_vec));
+                }
+                shell_vec.push(Boundaries::Nested(surface_vec));
+            }
+            if shell_vec.len() == 1 {
+                shell_vec.into_iter().next().unwrap()
+            } else {
+                Boundaries::Nested(shell_vec)
+            }
+        }
+        else if !self.surfaces.is_empty() {
+            let mut surface_vec = Vec::new();
+            for &rings_count in &self.surfaces {
+                let mut ring_vec = Vec::new();
+                for _ in 0..rings_count {
+                    let ring_size = self.strings[ring_cursor] as usize;
+                    ring_cursor += 1;
+                    let ring_indices = self.indices[index_cursor..index_cursor + ring_size as usize]
+                        .iter()
+                        .map(|x| *x as usize)
+                        .collect::<Vec<_>>();
+                    index_cursor += ring_size as usize;
+
+                    ring_vec.push(Boundaries::Indices(ring_indices.into_iter().map(|x| x as u32).collect()));
+                }
+                surface_vec.push(Boundaries::Nested(ring_vec));
+            }
+            if surface_vec.len() == 1 {
+                surface_vec.into_iter().next().unwrap()
+            } else {
+                Boundaries::Nested(surface_vec)
+            }
+        }
+        else if !self.strings.is_empty() {
+            let mut ring_vec = Vec::new();
+            for &ring_size in &self.strings {
+                let ring_indices = self.indices[index_cursor..index_cursor + ring_size as usize]
+                    .iter()
+                    .map(|x| *x as usize)
+                    .collect::<Vec<_>>();
+                index_cursor += ring_size as usize;
+                ring_vec.push(Boundaries::Indices(ring_indices.into_iter().map(|x| x as u32).collect()));
+            }
+            if ring_vec.len() == 1 {
+                ring_vec.into_iter().next().unwrap()
+            } else {
+                Boundaries::Nested(ring_vec)
+            }
+        }
+        else {
+            Boundaries::Indices(self.indices.into_iter().map(|x| x as u32).collect())
+        }
     }
 }
