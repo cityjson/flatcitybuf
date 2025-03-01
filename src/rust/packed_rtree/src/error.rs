19a0391f77c5b207a22036abf3b94c373bd3cb26
HideBa <baba.papa1120.ba@gmail.com>
2025-03-01 21:26:21 +0100

    wip: cause integer overflow error in packedrtree

--- src/rust/packed_rtree/src/error.rs
+++ src/rust/packed_rtree/src/error.rs
@@ -7,4 +7,13 @@ pub enum Error {
 
     #[error("http error: {0}")]
     Http(#[from] http_range_client::HttpError),
+
+    #[error("rtree error: {0}")]
+    RTreeError(String),
+}
+
+impl From<&str> for Error {
+    fn from(s: &str) -> Self {
+        Error::RTreeError(s.to_string())
+    }
 }
