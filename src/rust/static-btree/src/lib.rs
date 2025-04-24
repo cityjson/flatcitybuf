d0d2c09cebb09fe350c6d8ff976a9df7e6285245
HideBa <baba.papa1120.ba@gmail.com>
2025-04-24 11:36:15 +0200

    add mock client

--- src/rust/static-btree/src/lib.rs
+++ src/rust/static-btree/src/lib.rs
@@ -3,13 +3,15 @@ use std::mem;
 pub mod entry;
 pub mod error;
 pub mod key;
+mod mocked_http_range_client;
+pub mod payload;
 pub mod stree;
 
 pub use entry::Entry;
 pub use error::Error;
 pub use key::Key;
+pub use payload::PayloadEntry;
 pub use stree::Stree;
-pub mod payload;
 
 // Add basic tests or examples here later if needed
 #[cfg(test)]
