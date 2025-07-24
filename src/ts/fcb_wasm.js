70e1bf2dc1bab20a9e77ea596e2b369f111316ac
HideBa <baba.papa1120.ba@gmail.com>
2025-07-24 10:52:49 +0200

    fix: clean up wasm and built ts

--- src/ts/fcb_wasm.js
+++ src/ts/fcb_wasm.js
@@ -960,7 +960,7 @@ function __wbg_get_imports() {
         const ret = false;
         return ret;
     };
-    imports.wbg.__wbindgen_closure_wrapper1230 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper1228 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 328, __wbg_adapter_52);
         return ret;
     };
